a716b71f61f1af6ea67df0726850befd079c8881
emiliabrgf <burggrafemilia@gmail.com>
2021-03-14 20:47:55 +0100

    add try and catch for calculator

--- Assets/MyGame/Scripts/Controller.cs
+++ Assets/MyGame/Scripts/Controller.cs
@@ -2,33 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+    public InputField iFieldA;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public InputField iFieldB;
 
-    public void ChangeToColorScene()
-    {
-        SceneManager.LoadScene("01ColorScene");
-    }
+    public float finalWert;
 
-    public void ChangeToNumberScene()
-    {
-        SceneManager.LoadScene("02NumberScene");
-    }
+    public Text sumtxt;
 
+    public Text Error;
+
+    public Color red = Color.red;
+
+    public Color white = Color.white;
+
+    public float InputAFloat;
+
+    public float InputBFloat;
+
+    public void LoadNextSceneAutomated()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
     public void ChangeToWelcomeScene()
     {
         SceneManager.LoadScene("00WelcomeScene");
     }
+    public void ButtonAdd()
+    {
+        try
+        {
+            InputAFloat = float.Parse(iFieldA.text);
+            iFieldA.image.color = white;
+        }
+
+        catch(System.Exception)
+        {
+            iFieldA.image.color = red;
+            Error.text = "Error! Enter A Number!";
+        }
+
+        try
+        {
+            InputBFloat = float.Parse(iFieldB.text);
+            iFieldB.image.color = white;
+        }
+
+        catch (System.Exception)
+        {
+            iFieldB.image.color = red;
+            Error.text = "Error! Enter A Number!";
+        }
+
+        finalWert = float.Parse(iFieldA.text) + float.Parse(iFieldB.text);
+        Debug.Log(finalWert);
+        sumtxt.text = finalWert.ToString();
+    }
+
 }
